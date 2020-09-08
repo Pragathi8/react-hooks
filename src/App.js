2d1069fa1f0a827a9bdceb3443a640ec0c24a2ef
Pragathi Nataraj <pragathi.nataraj@publicissapient.com>
2020-09-08 11:30:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import Auth from "./components/Auth";
+import Ingredients from "./components/Ingredients/Ingredients";
+import { AuthContext } from "./context/auth-context";
+
+const App = props => {
+  const authContext = useContext(AuthContext);
+  return !authContext.isAuth ? <Auth /> : <Ingredients />;
+};
+
+export default App;
